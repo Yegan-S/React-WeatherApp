dbecc92d80b82d40182fe4129305b5e483038b12
sedi <salehisediqe86@gmail.com>
2023-05-18 06:07:54 -0700

    Added bootstrap and removed unnecessary things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+    return( 
+        <div className="App">
+            <div className="container">
+           <h1>Weather app</h1> 
+           <footer>
+            This project was built by {" "}
+            <a href="https://benevolent-druid-a3543f.netlify.apphttps://benevolent-druid-a3543f.netlify.app/" target="_blank" rel="noreferrer">Yegan Salehi </a>
+            and is {" "}
+            <a href="https://github.com/sedi-yegan/react-vanilla-weather-app" target="_blank" rel="noreferrer"> open-sourced on Github.</a>
+           </footer>
+        </div>
+        </div>
+    )
+    ;
 }
-
-export default App;
